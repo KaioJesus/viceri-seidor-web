6743e705790e1686b45204e7b4e10dade2bda1c8
Kaio <kaioramos990@hotmail.com>
2025-10-28 16:04:32 -0300

    fix: routing

--- src/app/pages/logged-page/logged-page.routes.ts
+++ src/app/pages/logged-page/logged-page.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoggedPageComponent } from './logged-page.component';
 import { authGuard } from '../../core/guards/auth.guard';
+import { PessoasComponent } from './components/pessoas/pessoas.component';
+import { Agenda } from './components/agenda/agenda';
+import { PlanosDeAcao } from './components/planos-de-acao/planos-de-acao';
+import { pessoasRoutes } from './components/pessoas/pessoas.route';
 
 
 export const loggedPageRoutes: Routes = [
@@ -10,19 +14,30 @@ export const loggedPageRoutes: Routes = [
         import('./logged-page.component').then(m => m.LoggedPageComponent),
       canActivate:[authGuard],
       children: [
-        // {
-        //   path: 'eventos/details/:id',
-        //    loadComponent: () =>
-        //   import(
-        //     './events/detail-events/detail-events.component'
-        //   ).then(m => m.DetailEventsComponent),
-        //   title: 'Detalhes do evento',
-        // },
-        // {
-        //   path: 'planos-de-leitura/details/:id',
-        //   loadComponent: ()=> import('./reading-plans/details-reading-plans/details-reading-plans.component').then(m => m.DetailsReadingPlansComponent),
-        //   title: 'Detalhes do plano de leitura'
-        // },
+        {
+          path: 'pessoas',
+           loadChildren: () =>
+          import(
+            './components/pessoas/pessoas.route'
+          ).then(m => m.pessoasRoutes),
+          title: 'Pessoas',
+        },
+        {
+          path: 'agenda',
+           loadComponent: () =>
+          import(
+            './components/agenda/agenda'
+          ).then(m => m.Agenda),
+          title: 'Agenda',
+        },
+        {
+          path: 'planos-de-acao',
+           loadComponent: () =>
+          import(
+            './components/planos-de-acao/planos-de-acao'
+          ).then(m => m.PlanosDeAcao),
+          title: 'Planos de ação',
+        },
       ]
     },
   {
