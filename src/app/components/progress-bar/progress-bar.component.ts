d5f7046c7abc2ac2029bef7e7679e71a192ef864
Kaio <kaioramos990@hotmail.com>
2025-10-28 16:02:48 -0300

    style: mudança na progressbar

--- src/app/components/progress-bar/progress-bar.component.ts
+++ src/app/components/progress-bar/progress-bar.component.ts
@@ -1,38 +1,40 @@
-import { CommonModule } from "@angular/common";
-import { Component, Input } from "@angular/core";
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-progress-bar',
   template: `
-  <div class="relative">
-    <div
-      [ngClass]="{
-        'bg-primary-medium text-white': progress !== 'notstarted',
-        'bg-neutral-gray25 text-neutral-gray75': progress === 'notstarted'
-      }"
-      class="flex h-[34px] w-[34px] items-center justify-center rounded-full font-semibold">
-      {{ step }}
-    </div>
+    <div class="relative">
+      <div
+        [ngClass]="{
+          'bg-primary-medium text-white': progress !== 'notstarted',
+          'bg-neutral-gray25 text-neutral-gray75': progress === 'notstarted'
+        }"
+        [style.color]="progress === 'inprogress' ? '#000000' : '#6D6D6D'"
+        class="flex h-[34px] w-[34px] items-center justify-center rounded-full font-semibold bg-[#F3F4F6]"
+      >
+        {{ step }}
+      </div>
 
-    <span class="absolute top-full mt-2 text-sm whitespace-nowrap"
-    [ngClass]="{
-      'left-0': step === 1,
-      'left-1/2 -translate-x-1/2': step === 2 || step === 3,
-      'right-0': step === 4,
-      'font-bold': progress === 'inprogress' || progress === 'completed'
-    }"
-    [style.color]="progress === 'inprogress' ? '#2A5EC9' : '#6D6D6D'">
-    {{ stepTitle }}
-  </span>
-  </div>
+      <span
+        class="absolute top-full mt-2 text-sm whitespace-nowrap"
+        [ngClass]="{
+          '': step === 1,
+          'left-1/2 -translate-x-1/2': step === 2 || step === 3,
+          'right-0': step === 4,
+          'font-bold': progress === 'inprogress' || progress === 'completed'
+        }"
+        [style.color]="progress === 'inprogress' ? '#4B5563' : '#6D6D6D'"
+      >
+        {{ stepTitle }}
+      </span>
+    </div>
   `,
   imports: [CommonModule],
 })
 export class ProgressBarComponent {
-
   @Input() progress: 'completed' | 'inprogress' | 'notstarted' = 'notstarted';
   @Input() step: number = 1;
   @Input() stepTitle: string = '';
   @Input() isLast: boolean = false;
-
 }
